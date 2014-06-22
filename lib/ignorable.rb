478b50b26fb4e4a6233b46366dc80cd5c529f868
Toby Hsieh <thrasher13b@gmail.com>
2014-06-22 14:00:03 -0700

    Ignore columns in subclasses too

--- lib/ignorable.rb
+++ lib/ignorable.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'active_support/core_ext/class/attribute'
 
 module Ignorable
   extend ActiveSupport::Concern
@@ -7,12 +8,14 @@ module Ignorable
     super.reject{|col| self.class.ignored_column?(col)}
   end
 
+  included do
+    class_attribute :ignored_columns
+  end
+
   module ClassMethods
     def columns # :nodoc:
       @columns ||= super.reject{|col| ignored_column?(col)}
     end
-
-    attr_reader :ignored_columns
 
     # Prevent Rails from loading a table column.
     # Useful for legacy database schemas with problematic column names,
@@ -24,10 +27,11 @@ module Ignorable
     #
     #   Topic.new.respond_to?(:attributes) => false
     def ignore_columns(*columns)
-      @ignored_columns ||= []
-      @ignored_columns += columns.map(&:to_s)
+      self.ignored_columns ||= []
+      self.ignored_columns += columns.map(&:to_s)
       reset_column_information
-      @ignored_columns.tap(&:uniq!)
+      descendants.each(&:reset_column_information)
+      self.ignored_columns.tap(&:uniq!)
     end
     alias ignore_column ignore_columns
 
@@ -35,13 +39,13 @@ module Ignorable
     # Accepts both ActiveRecord::ConnectionAdapter::Column objects,
     # and actual column names ('title')
     def ignored_column?(column)
-      ignored_columns.present? && ignored_columns.include?(
+      self.ignored_columns.present? && self.ignored_columns.include?(
         column.respond_to?(:name) ? column.name : column.to_s
       )
     end
 
     def reset_ignored_columns
-      @ignored_columns = []
+      self.ignored_columns = []
       reset_column_information
     end
   end
